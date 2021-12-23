10f3be43406148e53a482e091932bc33a2fbe59d
okolobaha-me <yevheniy.onyshchenko@gmail.com>
2021-12-23 17:09:05 +0200

    7th homework done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const createGallery = (list) => {
+  return list
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                  <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                  />
+              </a>
+          </div>`
+    )
+    .join("");
+};
+
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = createGallery(galleryItems);
+
+const link = document.querySelectorAll(".gallery__link");
+
+let modal = {};
+
+const modalClose = (e) => {
+  if (e.code === "Escape") {
+    modal.close(() => window.removeEventListener("keydown", modalClose));
+  }
+};
+
+link.forEach((l) => {
+  l.onclick = (e) => {
+    e.preventDefault();
+    const href = l.href;
+    const alt = l.querySelector("img").alt;
+    modal = basicLightbox.create(
+      `
+		<img width="1400" height="900" src="${href}" alt = "${alt}">
+	`
+    );
+
+    modal.show();
+    window.addEventListener("keydown", modalClose);
+  };
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const createGallery = (list) => {
+  return list
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                  <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                  />
+              </a>
+          </div>`
+    )
+    .join("");
+};
+
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = createGallery(galleryItems);
+
+const links = document.querySelectorAll("a");
+
+let lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+const onClickImg = (e) => {
+  e.preventDefault();
+};
+
+links.forEach((link) => link.addEventListener("click", onClickImg));
